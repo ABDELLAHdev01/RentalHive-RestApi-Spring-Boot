e3999d0d516cb57ca02bbe2ab412a24df4bc685e
yassine <ahmed.yassin.elhamzaoui2019@gmail.com>
2023-11-13 20:33:57 +0100

    create Rent entity with necessary relationships

---
+++ src/main/java/com/app/project/model/Rent.java
@@ -0,0 +1,39 @@
+package com.app.project.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Data;
+import java.util.Date;
+import java.util.List;
+
+
+@Data
+@Entity
+@Table(name ="rents")
+public class Rent {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private Date returnDate;
+	private Date rentDate;
+	
+	@ManyToOne
+	@JoinColumn(name ="user_id")
+	private User user;
+	
+	@ManyToMany
+    @JoinTable(
+        name = "rent_equipment",  
+        joinColumns = @JoinColumn(name = "rent_id"),  
+        inverseJoinColumns = @JoinColumn(name = "equipment_id")
+    )
+    private List<Equipment> equipments;
+}
