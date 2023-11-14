31f97aa3c7faf228fb66a02e042d4e0675f02626
yassine <ahmed.yassin.elhamzaoui2019@gmail.com>
2023-11-14 22:00:38 +0100

    create searchEquipment method to search an equipment using name or status in Equipment reppository

--- src/main/java/com/app/project/repository/EquipmentRepository.java
+++ src/main/java/com/app/project/repository/EquipmentRepository.java
@@ -1,6 +1,10 @@
 package com.app.project.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.app.project.model.Equipment;
@@ -9,5 +13,7 @@ import com.app.project.model.Equipment;
 @Repository
 public interface EquipmentRepository extends JpaRepository<Equipment, Long> {
 
+	@Query("SELECT e FROM Equipment e WHERE e.name LIKE %:name% OR e.status = :status")
+	public List<Equipment> searchEquipment(@Param("name") String name, @Param("status") boolean status);
 
 }
