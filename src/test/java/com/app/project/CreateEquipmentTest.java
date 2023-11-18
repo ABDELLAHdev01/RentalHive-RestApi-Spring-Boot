3dfb0ffaaccd6a3c226677b5010e05af904eaaca
ABDELLAHdev01 <abdellahelghoulamdev@gmail.com>
2023-11-18 19:54:03 +0100

    Test for valide Create Equipment

--- src/test/java/com/app/project/CreateEquipmentTest.java
+++ src/test/java/com/app/project/CreateEquipmentTest.java
@@ -4,6 +4,7 @@ import com.app.project.model.Equipment;
 import com.app.project.repository.EquipmentRepository;
 import com.app.project.service.EquipmentService;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -19,7 +20,33 @@ public class CreateEquipmentTest {
 
     @Test
     void CreateVaalidEquipment() {
-    // test create funciton
+        Equipment equipment = Equipment.builder()
+                .id(2l)
+                .price(23d)
+                .status("available")
+                .registration_number("bbb3")
+                .build();
+        Mockito.when(equipmentRepository.save(equipment)).thenReturn(equipment);
+        equipmentService.createEquipment(equipment,"yy");
+        Mockito.verify(equipmentRepository).save(equipment);
+
+    }
+
+
+    @Test
+    void UpdateEquipment(){
+        Equipment equipment = new Equipment();
+        equipment.setPrice(22d);
+        equipment.setName("dd");
+        equipment.setStatus("PENDING");
+
+        // Mock the save method of the equipmentRepository
+        Mockito.when(equipmentRepository.save(equipment)).thenReturn(equipment);
+
+        equipmentService.updateEquipment(equipment,"yy");
+        Mockito.verify(equipmentRepository).save(equipment);
+
+
 
     }
 }
